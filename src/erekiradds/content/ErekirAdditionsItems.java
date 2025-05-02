427c1e51de976dbe70dd37bd94655caf90977f0d
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-05-02 08:59:43 +0200

    Update ErekirAdditionsItems.java

--- src/erekiradds/content/ErekirAdditionsItems.java
+++ src/erekiradds/content/ErekirAdditionsItems.java
@@ -22,7 +22,7 @@ public class ErekirAdditionsItems {
         }};
 
         erekirAdditionsItems.addAll(
-                erekirCoal, staticGlass;
+                erekirCoal, staticGlass
         );
     }
 }

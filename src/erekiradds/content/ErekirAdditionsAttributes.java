0c0ba9f6baecd5b2b390af09c9010e42dcb1a1b9
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-05-02 16:24:29 +0200

    Update ErekirAdditionsAttributes.java

--- src/erekiradds/content/ErekirAdditionsAttributes.java
+++ src/erekiradds/content/ErekirAdditionsAttributes.java
@@ -5,13 +5,13 @@ import mindustry.world.meta.*;
 
 public class ErekirAdditionsAttributes {
     public static void load(){
-        Attribute.add("carbon-concentration");
+        Attribute.add("carbon");
     }
 
     public static void setAttributes(){
-        Blocks.graphiticWall.attributes.set(Attribute.get("carbon-concentration"), 1.75f);
-        Blocks.carbonWall.attributes.set(Attribute.get("carbon-concentration"), 1);
-        Blocks.arkyicWall.attributes.set(Attribute.get("carbon-concentration"), 0.75f);
-        Blocks.stoneWall.attributes.set(Attribute.get("carbon-concentration"), 0.5f);
+        Blocks.graphiticWall.attributes.set(Attribute.get("carbon"), 1.75f);
+        Blocks.carbonWall.attributes.set(Attribute.get("carbon"), 1);
+        Blocks.arkyicWall.attributes.set(Attribute.get("carbon"), 0.75f);
+        Blocks.stoneWall.attributes.set(Attribute.get("carbon"), 0.5f);
     }
 }

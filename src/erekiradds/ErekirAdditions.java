b51826840f9d68599e602cc657b8d8f1e682e991
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-05-02 08:52:24 +0200

    Create ErekirAdditions.java

---
+++ src/erekiradds/ErekirAdditions.java
@@ -0,0 +1,23 @@
+package erekiradds;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+import erekiradds.content.*;
+
+public class ErekirAdditions extends Mod{
+
+    public ErekirAdditions(){
+        Log.info("decided to add stuff to Erekir");
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("adding stuff to Erekir");
+        
+        ErekirAdditionsItems.load();
+        ErekirAdditionsBlocks.load();
+        ErekirAdditionsUnits.load();
+    }
+
+}
